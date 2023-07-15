4a43aceb83c562dc73fbffea05469c0168d747ca
Theodore Smith <tjs7321@gmail.com>
2023-07-15 04:25:13 -0400

    finished

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+
+
+
+
+/*function addingEventListener() {
+    return addEventListener('click', function() {
+        (alert('I was clicked!'))})
+}*/
+//const clickMeButton = document.getElementById('button')
+
+/*function addingEventListener(element) {
+    element.addEventListener('click', function() {
+        (alert('I was clicked!'))})
+}*/
+
+//addingEventListener(clickMeButton)
+
+//clickMeButton.addEventListener('click', function() {alert('I was clicked!')})
+
+//clickMeButton.onclick = () => (alert('I was clicked!'))
